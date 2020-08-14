bf9dd3761d5d2c7104696cbda89e166e592bbb84
Prashant Neupane <prashantneupane8848@gmail.com>
2020-08-14 18:09:19 -0500

    eeror

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ import cv2
 image = cv2.imread("background.jpg")
 (h, w, d) = image.shape
 print("width={}, height={}, depth={}".format(w, h, d))
+cv2.imshow("Image", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()  
