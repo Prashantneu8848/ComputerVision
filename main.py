8b4275bbc84492a898282195b54a85de2cafdf18
Prashant Neupane <prashantneupane8848@gmail.com>
2020-08-26 12:57:38 -0500

    Border detection using Sobel kernel

--- main.py
+++ main.py
@@ -17,29 +17,41 @@ print(image.dtype)
 print("width={}, height={}, depth={}".format(w, h, d))
 
 
-#Draw a diagonal line of 9 px in the image.
+# Draw a diagonal line of 9 px in the image.
 image = cv2.line(image, (0, 0), (w, h), (0, 255, 0), 9)
 
 # Only get the green channel.
 image = image[:,:,1]
 
-#Cropping the grayscale image
+# Cropping the grayscale image
 cropped = image[10:20, 100:200]
 
-#Adding a Gaussian blur filter to image.
+# Adding a Gaussian blur filter to image.
 hsize = (31, 31) # kernel size is 31 * 31
 sigma = 5
 blurimg = cv2.GaussianBlur(image, hsize, sigma)
 
-#Adding a median filter to image with kernel size 5.
+# Adding a median filter to image with kernel size 5.
 medianBlur = cv2.medianBlur(image, 5)
 
-#Matching template in the image.
+# Matching template in the image.
 img = image.copy()
 res = cv2.matchTemplate(img, cropped, cv2.TM_CCOEFF_NORMED)
 min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
 bottom_right = (max_loc[0] + w, max_loc[1] + h)
 cv2.rectangle(img, max_loc, bottom_right, 255, 2)
+
+'''
+Find x and y gradient using Sobel Kernel.
+
+The initial image is unit8. Black-to-White transition is taken as Positive slope
+while White-to-Black transition is taken as a Negative slope. So when it is converted to uint8,
+all negative slopes are made zero and the edge is lost. So, a higher form like 16s ot  64f is
+chosen.
+'''
+sobel_x_gradient = cv2.Sobel(image, cv2.CV_64F, 1, 0)
+sobel_y_gradient = cv2.Sobel(image, cv2.CV_64F, 0, 1)
+mag, direction = cv2.cartToPolar(sobel_x_gradient, sobel_y_gradient, angleInDegrees=True)
 
 imgrgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 plt.imshow(imgrgb)
