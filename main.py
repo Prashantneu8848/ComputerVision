2a81d61963ae86db3bab851d759426f4ec3f3a8e
Prashant Neupane <prashantneupane8848@gmail.com>
2020-10-13 20:17:41 -0500

    display template matching

--- main.py
+++ main.py
@@ -42,9 +42,11 @@ cropped = image[10:20, 100:200]
 hsize = (31, 31) # kernel size is 31 * 31
 sigma = 5
 blurimg = cv2.GaussianBlur(image, hsize, sigma)
+display_image(blurimg, 'image with gaussian blur')
 
 # Adding a median filter to image with kernel size 5.
 medianBlur = cv2.medianBlur(image, 5)
+display_image(medianBlur, 'image with median blur')
 
 # Matching template in the image.
 imgCopy = image.copy()
@@ -52,6 +54,7 @@ res = cv2.matchTemplate(imgCopy, cropped, cv2.TM_CCOEFF_NORMED)
 min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
 bottom_right = (max_loc[0] + w, max_loc[1] + h)
 cv2.rectangle(imgCopy, max_loc, bottom_right, 255, 2)
+display_image(imgCopy, 'image matching template from cropped image')
 
 '''
 Find x and y gradient using Sobel Kernel.
@@ -71,26 +74,6 @@ canny_edge = cv2.Canny(image, 100, 200)
 
 # Use Standard Hough Transform to find candidate for lines.
 lines = cv2.HoughLines(canny_edge, 1, np.pi / 180, 150, None, 0, 0)
-
-# TODO (me) add a picture with circles and use Hough Transform for Circles.
-
-# TODO (me) add stereo picture and show epipolar geometry between the pictures.
-
-# use Harris Corner Detection Algorithm to find corners in image.
-gray = cv2.cvtColor(imgCopy2, cv2.COLOR_BGR2GRAY)
-
-gray = np.float32(gray)
-dst = cv2.cornerHarris(gray, 2, 3, 0.04)
-#result is dilated for marking the corners, not important
-dst = cv2.dilate(dst, None)
-
-# Threshold for an optimal value, it may vary depending on the image.
-imgCopy2[dst>0.01*dst.max()]=[0,0,255]
-# imgCopy2rgb = cv2.cvtColor(imgCopy2, cv2.COLOR_BGR2RGB)
-display_image(imgCopy2, 'harris corner detection')
-
-# TODO (me) use SIFT to find corners in image.
-
 # Draw the lines
 if lines is not None:
     for i in range(0, len(lines)):
@@ -106,3 +89,21 @@ if lines is not None:
         cv2.line(canny_edge, pt1, pt2, (0,0,255), 3, cv2.LINE_AA)
 
 display_image(canny_edge, 'edge detection using canny edge algorithm')
+
+# TODO (me) add a picture with circles and use Hough Transform for Circles.
+
+# TODO (me) add stereo picture and show epipolar geometry between the pictures.
+
+# TODO (me) use SIFT to find corners in image.
+
+
+# use Harris Corner Detection Algorithm to find corners in image.
+gray = cv2.cvtColor(imgCopy2, cv2.COLOR_BGR2GRAY)
+gray = np.float32(gray)
+dst = cv2.cornerHarris(gray, 2, 3, 0.04)
+#result is dilated for marking the corners, not important
+dst = cv2.dilate(dst, None)
+# Threshold for an optimal value, it may vary depending on the image.
+imgCopy2[dst>0.01*dst.max()]=[0,0,255]
+# imgCopy2rgb = cv2.cvtColor(imgCopy2, cv2.COLOR_BGR2RGB)
+display_image(imgCopy2, 'harris corner detection')
