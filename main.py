6a4ca4d72694ed5ee125f3cd5b649c007a3e8cd8
Prashant Neupane <prashantneupane8848@gmail.com>
2020-11-03 05:49:35 -0600

    display all images

--- main.py
+++ main.py
@@ -47,20 +47,21 @@ print("width={}, height={}, depth={}".format(w, h, d))
 
 # Only get the green channel.
 image = image[:, :, 1]
-# display_image(image, 'image only with green channel')
+display_image(image, 'image only with green channel')
 
 # Cropping the grayscale image
 cropped = image[10:12, 100:105]
+display_image(image, 'cropped green channel image')
 
 # Adding a Gaussian blur filter to image.
 hsize = (31, 31)  # kernel size is 31 * 31
 sigma = 5
 blurimg = cv2.GaussianBlur(image, hsize, sigma)
-# display_image(blurimg, 'image with gaussian blur')
+display_image(blurimg, 'image with gaussian blur')
 
 # Adding a median filter to image with kernel size 5.
 medianBlur = cv2.medianBlur(image, 5)
-# display_image(medianBlur, 'image with median blur')
+display_image(medianBlur, 'image with median blur')
 
 # Matching template in the image.
 imgCopy = image.copy()
@@ -68,7 +69,7 @@ res = cv2.matchTemplate(imgCopy, cropped, cv2.TM_CCOEFF_NORMED)
 min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
 bottom_right = (max_loc[0] + w, max_loc[1] + h)
 cv2.rectangle(imgCopy, max_loc, bottom_right, 255, 2)
-# display_image(imgCopy, 'image matching template from cropped image')
+display_image(imgCopy, 'image matching template from cropped image')
 
 '''
 Find x and y gradient using Sobel Kernel.
@@ -86,7 +87,7 @@ mag, direction = cv2.cartToPolar(
 # Edge detection using Canny Edge Detection Algorithm which has magnitude
 # threshold of 100 min and 200 max.
 canny_edge = cv2.Canny(image, 100, 200)
-# display_image(canny_edge, 'edge detection using canny edge algorithm')
+display_image(canny_edge, 'edge detection using canny edge algorithm')
 
 # Use Standard Hough Transform to find candidate for lines.
 lines = cv2.HoughLines(canny_edge, 1, np.pi / 180, 150, None, 0, 0)
@@ -104,7 +105,7 @@ if lines is not None:
         # Add lines to the edges detected from Canny Edge.
         cv2.line(canny_edge, pt1, pt2, (0, 0, 255), 3, cv2.LINE_AA)
 
-# display_image(canny_edge, 'line detection using Hough Transformation')
+display_image(canny_edge, 'line detection using Hough Transformation')
 
 sift_img2 = cv2.imread("cropped.jpg")
 
@@ -138,4 +139,4 @@ dst = cv2.dilate(dst, None)
 # Threshold for an optimal value, it may vary depending on the image.
 imgCopy2[dst > 0.01*dst.max()] = [0, 0, 255]
 # imgCopy2rgb = cv2.cvtColor(imgCopy2, cv2.COLOR_BGR2RGB)
-# display_image(imgCopy2, 'harris corner detection')
+display_image(imgCopy2, 'harris corner detection')
